90e90a9cbed1bda3718f51c003497cd2d90c20a9
NusfaJahan61 <nusfaikra@gmail.com>
2025-11-05 22:34:52 +0600

    react router v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <>
-      <center>
-        <p style={{ fontSize: "40px" }}>Contact Manager</p>
-      </center>
-    </>
+    <div className='header'>
+      <p style={{ fontSize: "40px" }}>Contact Manager</p>
+      <div>
+        <Link to="/contacts" ><button>Go to Contacts</button></Link>
+        <Link to="/" ><button style={{ marginLeft: "10px" }}>Add Contact</button></Link>
+      </div>
+    </div>
   )
 }
 
